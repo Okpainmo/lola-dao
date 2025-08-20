18ea8c26faff71fce16ac5782bc7c2da1b2eafd6
Okpainmo <okpainmoandrew@gmail.com>
2025-08-20 11:58:58 +0100

    refactor: i_owner should be private - getting function will expose it

--- contracts/auth/OnlyOwnerAuth.sol
+++ contracts/auth/OnlyOwnerAuth.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.20;
 contract OnlyOwnerAuth {
     error OnlyOwnerAuth__OwnerOnly();
 
-    address public immutable i_owner;
+    address internal immutable i_owner;
 
     modifier onlyOwner(address _address) {
         if(_address != i_owner) {
