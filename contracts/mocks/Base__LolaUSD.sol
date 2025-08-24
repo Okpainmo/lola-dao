eebb9c6d0444ca2b9698ea728d9262254195e4a8
Okpainmo <okpainmoandrew@gmail.com>
2025-08-25 00:01:30 +0100

    chore: added token contracts(mocks) + for local deployment testing

---
+++ contracts/mocks/Base__LolaUSD.sol
@@ -0,0 +1,306 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+import "../interfaces/IAdminManagement__Base.sol";
+import "../interfaces/IProposalManagement__Base.sol";
+
+contract Base__LolaUSD {
+    error LolaUSD__InsufficientBalance();
+    error LolaUSD__OperatorAddressIsZeroAddress();
+    error LolaUSD__OwnerAddressIsZeroAddress();
+    error LolaUSD__ReceiverAddressIsZeroAddress();
+    error LolaUSD__TransferOperationWasUnsuccessful();
+    error LolaUSD__ExcessSpendRequestOrUnauthorized();
+    error LolaUSD__ExcessAllowanceDecrement();
+    error LolaUSD__UnsuccessfulTransferFromOperation();
+    error LolaUSD__ZeroAddressError();
+    error LolaUSD__AccessDenied_AdminOnly();
+    error LolaUSD__ProposalAlreadyExecuted();
+    error LolaUSD__InvalidProposalCodeName();
+
+    event Transfer(
+        address indexed _owner,
+        address indexed _receiver,
+        uint256 _value
+    );
+    event Approval(
+        address indexed _owner,
+        address indexed _operator,
+        uint256 _value
+    );
+
+    address internal s_adminManagementCoreContractAddress; // needed to check admin rights and likely more
+    address internal s_proposalManagementCoreContractAddress; // needed for interaction with the external proposals management core contract
+
+    IAdminManagement__Base internal s_adminManagementContract_Base =
+        IAdminManagement__Base(s_adminManagementCoreContractAddress);
+    IProposalManagement__Base internal s_proposalManagementContract_Base =
+        IProposalManagement__Base(s_proposalManagementCoreContractAddress);
+
+    string internal s_tokenName;
+    string internal s_tokenSymbol;
+    uint8 internal s_tokenDecimals;
+    uint256 internal s_supply;
+
+    mapping(address => uint256) internal balance;
+    mapping(address => mapping(address => uint256)) internal allowedSpend;
+
+    function name() public view returns (string memory) {
+        return s_tokenName;
+    }
+
+    function symbol() public view returns (string memory) {
+        return s_tokenSymbol;
+    }
+
+    function decimals() public view returns (uint8) {
+        return s_tokenDecimals;
+    }
+
+    function totalSupply() public view returns (uint256) {
+        return s_supply / (10 ** s_tokenDecimals);
+    }
+
+    function balanceOf(address _owner) public view returns (uint256) {
+        return balance[_owner] / (10 ** s_tokenDecimals);
+    }
+
+    function approve(address _operator, uint256 _value) public returns (bool) {
+        _value = _value * 10 ** s_tokenDecimals;
+
+        if (_operator == address(0)) {
+            revert LolaUSD__OperatorAddressIsZeroAddress();
+        }
+
+        allowedSpend[msg.sender][_operator] = _value; // user(msg.sender) allows platform/operator, e.g. a DEX, to currently be able to spend '_value' amount from their balance
+
+        emit Approval(msg.sender, _operator, _value);
+
+        return true;
+    }
+
+    function allowance(
+        address _owner,
+        address _operator
+    ) public view returns (uint256) {
+        return allowedSpend[_owner][_operator] / (10 ** s_tokenDecimals); // check how much a user has allowed an operator to spend
+    }
+
+    function increaseAllowance(
+        address _operator,
+        uint256 _amountToAdd
+    ) external returns (bool) {
+        _amountToAdd = _amountToAdd * 10 ** s_tokenDecimals;
+
+        if (_operator == address(0)) {
+            revert LolaUSD__OperatorAddressIsZeroAddress();
+        }
+
+        uint256 newAllowance = allowedSpend[msg.sender][_operator] +
+            _amountToAdd;
+
+        allowedSpend[msg.sender][_operator] = newAllowance;
+
+        emit Approval(msg.sender, _operator, newAllowance);
+
+        return true;
+    }
+
+    function decreaseAllowance(
+        address _operator,
+        uint256 _amountToDeduct
+    ) external returns (bool) {
+        _amountToDeduct = _amountToDeduct * 10 ** s_tokenDecimals;
+
+        if (_operator == address(0)) {
+            revert LolaUSD__OperatorAddressIsZeroAddress();
+        }
+
+        uint256 currentAllowance = allowedSpend[msg.sender][_operator];
+
+        if (_amountToDeduct > currentAllowance) {
+            revert LolaUSD__ExcessAllowanceDecrement();
+        }
+
+        uint256 newAllowance = currentAllowance - (_amountToDeduct);
+
+        allowedSpend[msg.sender][_operator] = newAllowance;
+
+        emit Approval(msg.sender, _operator, newAllowance);
+
+        return true;
+    }
+
+    function transfer(address _receiver, uint256 _value) public returns (bool) {
+        _value = _value * 10 ** s_tokenDecimals;
+
+        if (_receiver == address(0)) {
+            revert LolaUSD__ReceiverAddressIsZeroAddress();
+        }
+
+        if (_value > balance[msg.sender]) {
+            revert LolaUSD__InsufficientBalance();
+        }
+
+        balance[msg.sender] -= (_value);
+        balance[_receiver] += (_value);
+
+        emit Transfer(msg.sender, _receiver, _value);
+
+        return true;
+    }
+
+    function transferFrom(
+        address _owner,
+        address _receiver,
+        uint256 _value
+    ) public returns (bool) {
+        _value = _value * 10 ** s_tokenDecimals;
+
+        if (_receiver == address(0)) {
+            revert LolaUSD__ReceiverAddressIsZeroAddress();
+        }
+
+        if (_owner == address(0)) {
+            revert LolaUSD__OwnerAddressIsZeroAddress();
+        }
+
+        uint256 currentAllowance = allowedSpend[_owner][msg.sender];
+
+        if (currentAllowance < _value) {
+            revert LolaUSD__ExcessSpendRequestOrUnauthorized();
+        }
+
+        if (balance[_owner] < _value) {
+            revert LolaUSD__InsufficientBalance();
+        }
+
+        balance[_owner] -= _value;
+        balance[_receiver] += _value;
+
+        // standard “infinite approval” behavior: an added if block
+        // if (currentAllowance != type(uint256).max) {
+        //     allowedSpend[_owner][msg.sender] = currentAllowance - _value;
+        //     emit Approval(_owner, msg.sender, allowedSpend[_owner][msg.sender]); // optional but good practice
+        // }
+
+        allowedSpend[_owner][msg.sender] = currentAllowance - _value;
+        emit Approval(_owner, msg.sender, allowedSpend[_owner][msg.sender]); // optional but good practice
+        // allowedSpend[_owner][msg.sender] =  0;
+
+        emit Transfer(_owner, _receiver, _value);
+
+        return true;
+    }
+
+    function mint(
+        address _to,
+        uint256 _amount,
+        uint256 _proposalId,
+        string memory _proposalCodeName
+    ) public {
+        _amount = _amount * 10 ** s_tokenDecimals;
+
+        if (!s_adminManagementContract_Base.checkIsAdmin(msg.sender)) {
+            revert LolaUSD__AccessDenied_AdminOnly();
+        }
+
+        IProposalManagement__Base.Proposal
+            memory proposal = s_proposalManagementContract_Base.getProposalById(
+                _proposalId
+            );
+
+        if (
+            proposal.proposalStatus ==
+            IProposalManagement__Base.ProposalStatus.Executed
+        ) {
+            revert LolaUSD__ProposalAlreadyExecuted();
+        }
+
+        if (
+            keccak256(bytes(proposal.proposalCodeName)) !=
+            keccak256(bytes(_proposalCodeName))
+        ) {
+            revert LolaUSD__InvalidProposalCodeName();
+        }
+
+        if (_to == address(0)) {
+            revert LolaUSD__ReceiverAddressIsZeroAddress();
+        }
+
+        s_supply += _amount;
+
+        balance[_to] += _amount;
+
+        emit Transfer(address(0), _to, _amount);
+
+        // updates the status of the proposal in the external proposal-management core contract to "executed"
+        s_proposalManagementContract_Base.executeProposal(_proposalId);
+    }
+
+    function burn(
+        address _from,
+        uint256 _amount,
+        uint256 _proposalId,
+        string memory _proposalCodeName
+    ) public {
+        _amount = _amount * 10 ** s_tokenDecimals;
+
+        if (!s_adminManagementContract_Base.checkIsAdmin(msg.sender)) {
+            revert LolaUSD__AccessDenied_AdminOnly();
+        }
+
+        IProposalManagement__Base.Proposal
+            memory proposal = s_proposalManagementContract_Base.getProposalById(
+                _proposalId
+            );
+
+        if (
+            proposal.proposalStatus ==
+            IProposalManagement__Base.ProposalStatus.Executed
+        ) {
+            revert LolaUSD__ProposalAlreadyExecuted();
+        }
+
+        if (
+            keccak256(bytes(proposal.proposalCodeName)) !=
+            keccak256(bytes(_proposalCodeName))
+        ) {
+            revert LolaUSD__InvalidProposalCodeName();
+        }
+
+        if (_from == address(0)) {
+            revert LolaUSD__OwnerAddressIsZeroAddress();
+        }
+
+        if (balance[_from] < _amount) {
+            revert LolaUSD__InsufficientBalance();
+        }
+
+        if (_from == msg.sender) {
+            // if self(master or admin) address, proceed to burn directly
+            balance[_from] -= _amount;
+            s_supply -= _amount;
+
+            emit Transfer(msg.sender, address(0), _amount);
+        } else {
+            // if not self address, proceed to burn via allowance
+            uint256 currentAllowance = allowedSpend[_from][msg.sender];
+
+            if (currentAllowance < _amount) {
+                revert LolaUSD__ExcessSpendRequestOrUnauthorized();
+            }
+
+            balance[_from] -= _amount;
+            s_supply -= _amount;
+
+            allowedSpend[_from][msg.sender] = currentAllowance - _amount;
+            emit Approval(_from, msg.sender, allowedSpend[_from][msg.sender]);
+
+            emit Transfer(_from, address(0), _amount);
+        }
+
+        // updates the status of the proposal in the external proposal-management core contract to "executed"
+        s_proposalManagementContract_Base.executeProposal(_proposalId);
+    }
+}
+
---
+++ contracts/mocks/core/Core__LolaUSD.sol
@@ -0,0 +1,197 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "../Base__LolaUSD.sol";
+import "../../interfaces/IAdminManagement__Core.sol";
+
+contract Core__LolaUSD is Base__LolaUSD {
+    error LolaUSDCore__ZeroAddressError();
+    error LolaUSDCore__AccessDenied_AdminOnly();
+    error LolaUSDCore__LogoNameCannotBeEmpty();
+    error LolaUSDCore__SpendApprovalFailedForAirdropContract();
+    error LolaUSDCore__NonMatchingAdminAddress();
+
+    event Logs(string message, uint256 timestamp, string indexed contractName);
+
+    string private constant CONTRACT_NAME = "Core__LolaUSD"; // set in one place to avoid mispelling elsewhere
+    address private i_owner;
+    string private s_tokenImageCID;
+    string private s_tokenMetadataCID;
+    address internal s_airdropCoreContractAddress; // only needed for transferFrom approval - no contract initialization
+
+    // IBase__Airdrop internal airdropContract__Base = IBase__Airdrop(s_airdropCoreContractAddress);
+    uint256 internal s_airdropLimit;
+    uint256 internal s_airdropAmount;
+
+    constructor(
+        string memory _tokenName,
+        string memory _tokenLogoCID,
+        string memory _tokenMetadataCID,
+        string memory _tokenSymbol,
+        uint8 _decimals,
+        uint256 _supply,
+        address _adminManagementCoreContractAddress
+        // address _proposalManagementCoreContractAddress,
+    ) {
+        s_tokenName = _tokenName;
+        s_tokenSymbol = _tokenSymbol;
+        s_tokenDecimals = _decimals;
+        i_owner = msg.sender;
+        s_supply = _supply * 10 ** s_tokenDecimals;
+
+        s_tokenMetadataCID = _tokenMetadataCID;
+        s_tokenImageCID = _tokenLogoCID;
+
+        s_adminManagementCoreContractAddress = _adminManagementCoreContractAddress; // needed to check admin rights and likely more
+        // s_proposalManagementCoreContractAddress = _proposalManagementCoreContractAddress;
+
+        s_adminManagementContract_Base = IAdminManagement__Base(s_adminManagementCoreContractAddress);
+        // s_proposalManagementContract_Base = IProposalManagement__Base(s_proposalManagementCoreContractAddress);
+
+        balance[msg.sender] = s_supply; 
+        emit Transfer(address(0), msg.sender, s_supply);
+
+        emit Logs(
+            "contract deployed successfully with constructor chores completed",
+            block.timestamp,
+            CONTRACT_NAME
+        );
+    }
+
+    function getContractName() public pure returns (string memory) {
+        return CONTRACT_NAME;
+    }
+
+    function getContractOwner() public view returns (address) {
+        return i_owner;
+    }
+
+    function getAdminManagementCoreContractAddress()
+        public
+        view
+        returns (address)
+    {
+        return s_adminManagementCoreContractAddress;
+    }
+
+    function getProposalManagementCoreContractAddress()
+        public
+        view
+        returns (address)
+    {
+        return s_proposalManagementCoreContractAddress;
+    }
+
+    function getAirdropCoreContractAddress()
+        public
+        view
+        returns (address)
+    {
+        return s_airdropCoreContractAddress;
+    }
+
+    function updateAdminManagementCoreContractAddress(
+        address _newAddress
+    ) public {
+        if (!s_adminManagementContract_Base.checkIsAdmin(msg.sender)) {
+            revert LolaUSDCore__AccessDenied_AdminOnly();
+        }
+        
+        if (_newAddress == address(0)) {
+            revert LolaUSDCore__ZeroAddressError();
+        }
+
+        /* 
+        updating the admin management core contract address is a very sensitive process. The old/current contract 
+        to switch from can be active and working, but if the 'isAdmin' check is passed(on the old/current contract), 
+        and a new address is set which is wrong, it becomes impossible to now connect to the intending admin 
+        contract. Hence the next step of admin check below, will keep failing and impossible to pass due to contract 
+        immutability. Other chores requiring admin check will also be impossible.
+    
+        Hence the need to first connect and ping to make sure the new contract works before setting
+        */
+        // first connect and ping
+        IAdminManagement__Core s_adminManagementContract_BaseToVerify = IAdminManagement__Core(_newAddress);
+        ( , address contractAddress, ) = s_adminManagementContract_BaseToVerify.ping();
+
+        // the fact that it pings without an error is enough - but still do as below to be super-sure
+        if(contractAddress != _newAddress) { 
+            revert LolaUSDCore__NonMatchingAdminAddress();
+        }
+
+        /* also ensure current sender is an admin on that contract - which further verifies that the contract 
+        is indeed and 'adminManagement' contract */
+        if (!s_adminManagementContract_BaseToVerify.checkIsAdmin(msg.sender)) {
+            revert LolaUSDCore__AccessDenied_AdminOnly();
+        }
+
+        s_adminManagementCoreContractAddress = _newAddress;
+        s_adminManagementContract_Base = IAdminManagement__Base(s_adminManagementCoreContractAddress);
+    }
+    
+    // // not needed
+    // function updateAirdropCoreContractAddress( 
+    //     address _newAddress
+    // ) public {
+    //     if (!s_adminManagementContract_Base.checkIsAdmin(msg.sender)) {
+    //         revert LolaUSDCore__AccessDenied_AdminOnly();
+    //     }
+
+    //     if (_newAddress == address(0)) {
+    //         revert LolaUSDCore__ZeroAddressError();
+    //     }
+
+    //     s_airdropCoreContractAddress = _newAddress;
+    // }
+
+    function updateProposalManagementCoreContractAddress(
+        address _newAddress
+    ) public {
+        if (!s_adminManagementContract_Base.checkIsAdmin(msg.sender)) {
+            revert LolaUSDCore__AccessDenied_AdminOnly();
+        }
+
+        if (_newAddress == address(0)) {
+            revert LolaUSDCore__ZeroAddressError();
+        }
+
+        s_proposalManagementCoreContractAddress = _newAddress;
+        s_proposalManagementContract_Base = IProposalManagement__Base(s_proposalManagementCoreContractAddress);
+    }
+
+    function getTokenLogo() public view returns (string memory) {
+        return s_tokenImageCID;
+    }
+
+    function getTokenMetadata() public view returns (string memory) {
+        return s_tokenMetadataCID;
+    }
+
+    function updateTokenLogo(string memory _newLogoCID) public {
+        if (!s_adminManagementContract_Base.checkIsAdmin(msg.sender)) {
+            revert LolaUSDCore__AccessDenied_AdminOnly();
+        }
+
+        if (bytes(_newLogoCID).length < 1)
+            revert LolaUSDCore__LogoNameCannotBeEmpty();
+
+        s_tokenImageCID = _newLogoCID;
+    }
+
+    function updateTokenMetaData(string memory _newMetaDataCID) public {
+        if (!s_adminManagementContract_Base.checkIsAdmin(msg.sender)) {
+            revert LolaUSDCore__AccessDenied_AdminOnly();
+        }
+
+        if (bytes(_newMetaDataCID).length < 1)
+            revert LolaUSDCore__LogoNameCannotBeEmpty();
+
+        s_tokenMetadataCID = _newMetaDataCID;
+    }
+
+    function ping() external view returns (string memory, address, uint256) {
+        return (CONTRACT_NAME, address(this), block.timestamp);
+    }
+}
+
+
